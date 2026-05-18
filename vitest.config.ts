35dcf091a7d1e1ed799ea28ab0958046d2e63fef
sumitroajiprabowo <bowo@anakdesa.id>
2026-05-19 03:28:09 +0700

    chore: add vitest.config.ts

---
+++ vitest.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+	test: {
+		coverage: {
+			provider: "v8",
+			reporter: ["text", "lcov"],
+			include: ["src/**/*.ts"],
+			exclude: ["src/types.ts"],
+			thresholds: {
+				branches: 100,
+				functions: 100,
+				lines: 100,
+				statements: 100,
+			},
+		},
+	},
+});
